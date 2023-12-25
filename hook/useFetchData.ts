ea63e53a5b2270dcbf3062ee4459414443556c85
Hac-Phong <quyet24042002@gmail.com>
2023-12-25 14:53:36 +0700

    Refactor useFetchData to use await for res.json()

--- hook/useFetchData.ts
+++ hook/useFetchData.ts
@@ -3,7 +3,7 @@ export const useFetchData = async (path: string): Promise<any> => {
     const res = await fetch(`https://comics-api.vercel.app${path}`, {
       cache: 'force-cache',
     })
-    const data = res.json()
+    const data = await res.json()
     return data
   } catch (error) {
     console.log(error)
